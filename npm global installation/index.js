376a1b9d999e4566d1b3731573dc17c0079bfd1b
sowmiya637 <sowmiyaravichandran637@gmail.com>
2025-11-18 17:51:38 +0530

    Add files via upload

---
+++ npm global installation/index.js
@@ -0,0 +1,13 @@
+// Simple Node.js App Example
+const http = require('http');
+
+// Create a simple server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Sowmiya! Welcome to Node.js ');
+});
+
+// Start the server
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
