62751493e945d6a1af23663248c42b65a75cc87f
sowmiya637 <sowmiyaravichandran637@gmail.com>
2025-11-18 17:54:33 +0530

    Delete scripts directory

--- scripts/watch-client.js
+++
@@ -1,6 +0,0 @@
-// scripts/watch-client.js
-console.log('Watching client files (simulated). Save files to trigger rebuild...');
-// simple interval to show it is "watching"
-setInterval(() => {
-  process.stdout.write('.');
-}, 2000);
